64d47a3c6e207971f6e302a5b3caaf13ef45f298
Davide Fissore <fisso9900@gmail.com>
2026-02-24 15:27:26 +0100

    style

--- code/base_tc_hs.hs
+++ code/base_tc_hs.hs
@@ -2,10 +2,8 @@ integerEq :: Integer -> Integer -> Bool
 integerEq x y = x == y
 
 -- START
-class Eqb a where
-  eqb :: a -> a -> Bool
+class Eqb a where eqb :: a -> a -> Bool
 
 instance Eqb Integer where eqb = integerEq
-
-instance (Eqb a, Eqb b)=> Eqb (a, b) where
+instance (Eqb a, Eqb b) => Eqb (a, b) where
   eqb (x1, y1) (x2, y2) = eqb x1 x2 && eqb y1 y2
