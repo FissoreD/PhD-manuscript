3c1958ac9c3b073e8dc1dd41145a9d0e35c2b816
Davide Fissore <fisso9900@gmail.com>
2026-03-03 19:14:29 +0100

    more tc

--- code/base_tc.v
+++ code/base_tc.v
@@ -1,8 +1,9 @@
 From mathcomp Require Import all_ssreflect.
+Set Implicit Arguments.
 
 (* START *)
-Class Eqb (T : Type) := {eqb : T -> T -> bool}.
+Class Eqb T := {eqb : T -> T -> bool}.
 
 Instance eqNat : Eqb nat := {eqb a b := Nat.eqb a b}.
-Instance eqProd (A: Type) (B: Type) : Eqb A -> Eqb B -> Eqb (A * B) :=
-  {eqb a b := eqb a.1 b.1 && eqb a.2 b.2}.
+Instance eqProd T1 T2 : Eqb T1 -> Eqb T2 -> Eqb (T1 * T2) :=
+  {eqb '(x1,y1) '(x2, y2) := eqb x1 x2 && eqb y1 y2}.
