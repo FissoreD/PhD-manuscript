5f295498aa424f67ea1c378e381a8e4a5847e7d2
Davide Fissore <fisso9900@gmail.com>
2026-03-05 11:54:09 +0100

    elpi syntax

--- code/main.py
+++ code/main.py
@@ -1,10 +1,17 @@
 import extract_code, sys
 
+def remove_quote(l):
+    sym = ["cons", "nil", "list"]
+    for i in sym:
+        l = l.replace(i+"'", i)
+    return l
+
 def clean_line(escape):
     def f(l):
         l = l.replace("¬", "~$\lnot$~")
         l = l.replace("forall", "~$\\forall$~")
         l = l.replace("<->", "~$\\leftrightarrow$~")
+        l = remove_quote(l)
         for i in range(10):
             l = l.replace(f"t{i}", f"~$t_{i}$~")
         return l
